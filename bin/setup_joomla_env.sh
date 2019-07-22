006ade0e925e61b8ba9bd64d5d921ee36bc9adba
Marko Vucen <marko_vucen@hotmail.com>
2019-07-22 10:32:46 +0200

    Add export composer bin path to system $PATH

--- bin/setup_joomla_env.sh
+++ bin/setup_joomla_env.sh
@@ -6,4 +6,8 @@ composer global require joomlatools/console
 # Setup PHP CodeSniffer https://docs.joomla.org/Joomla_CodeSniffer
 composer global require squizlabs/php_codesniffer "~2.8"
 composer global require joomla/coding-standards "~2.0@alpha"
+
+# Make sure we are added composer bin to PATH
+export PATH="$PATH:$HOME/.composer/vendor/bin"
+
 phpcs --config-set installed_paths "$HOME/.composer/vendor/joomla/coding-standards"
